d929c15ffd24ffac402bffb2cf744c4fbd031a71
shreyash101 <shr.shawn@gmail.com>
2022-10-08 23:18:13 +0530

    added javascript
    renders using javascript. however, no functionality for storing, incrementing likes.

--- index.js
+++ index.js
@@ -28,3 +28,29 @@ const posts = [
     }
 ]
 
+const container = document.getElementById("container");
+let section = "";
+for (let i = 0; i < posts.length; i++) {
+    section += `<section>
+                        <div class="avatar-info">
+                            <img class="circle" src=${posts[i].avatar} alt="avatar">
+                            <div>
+                                <h2>${posts[i].name}</h2>
+                                <p>${posts[i].location}</p>
+                            </div>
+                        </div>
+                        <img class="post" src=${posts[i].post} alt="post">
+                        <div class="post-info">
+                            <div class="icons-container">
+                                <img class="icons" src="images/icon-heart.png" alt="like icon">
+                                <img class="icons" src="images/icon-comment.png" alt="comment icon">
+                                <img class="icons" src="images/icon-dm.png" alt="dm icon">
+                            </div>
+                            <p class="dark">${posts[i].likes} likes</p>
+                            <p><span class="dark">${posts[i].username}</span> ${posts[i].comment}</p>
+                        </div>
+                        <div class="space"></div>
+                   </section>`
+}
+
+container.innerHTML += section
